d53f8e2284f266cf99618296a495eeb262324054
b03902061 <b03902061@ntu.edu.tw>
2017-01-15 01:10:40 +0800

    add test into ML_sk

--- src/ML_sk.py
+++ src/ML_sk.py
@@ -1,4 +1,4 @@
-# python 2
+# python 2, sklearn LR
 # take one crossroad as one training data
 
 import numpy as np
@@ -11,6 +11,9 @@ features_idx_PM = range(5, 11) + range(33, 53)
 # input
 X = []
 Y = []
+# test
+tX = []
+tY = []
 
 # get data, make feature matrix 
 # only one year's AM for now
@@ -21,29 +24,40 @@ with open("../new/info_reduced.txt") as f:
     for line in f:
         features = (line[:-1]).split(',')
         X.append([int(features[i]) for i in range(len(features)) if i in features_idx_AM])
-        #X.append([int(features[i]) for i in range(len(features)) if i in features_idx_PM])
+        tX.append([int(features[i]) for i in range(len(features)) if i in features_idx_PM])
 
 # get data, make label matrix
 with open('../new/102_count_reduced.txt') as f:
     f.readline()
     for line in f:
         Y.append(int(line[:-1].split(',')[1]))
-        #Y.append(int(line[:-1].split(',')[2]))
+        tY.append(int(line[:-1].split(',')[2]))
 
 feature_size = len(features_idx_AM)
 X = np.array(X, dtype=np.float32)
 Y = np.array(Y, dtype=np.float32)
 Y = Y / Y.sum()
+tX = np.array(tX, dtype=np.float32)
+tY = np.array(tY, dtype=np.float32)
+tY = tY / tY.sum()
 print X
 print Y
 print X.shape, Y.shape
 print ''
 
-# sklearn
 regr = linear_model.LinearRegression()
 regr.fit(X, Y)
 #print 'coefficients:', regr.coef_
-pred = regr.predict(X)
-print 'predict:\n', pred, pred.sum()
-print 'error:', np.abs(pred - Y).sum()
+
+# predict training data
+pred_train = regr.predict(X)
+print 'possibilities of train (AM):\n', Y
+print 'predict of train (AM):\n', pred_train, pred_train.sum()
+print 'error:', np.abs(pred_train - Y).sum(), '\n'
+
+# predict test data
+pred_test = regr.predict(tX)
+print 'possibilities of test(PM):\n', tY
+print 'predict of test (PM):\n', pred_test, pred_test.sum()
+print 'error:', np.abs(pred_test - tY).sum(), '\n'
 
